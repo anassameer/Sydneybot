f0c4edc9eefeb8ec285730f1efe23b6847bedd77
anassameer <139580401+anassameer@users.noreply.github.com>
2023-11-03 17:13:01 +0530

    Ready to deploy

---
+++ sample_info.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Sydney'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = 'e9813006985c0f6819f54eb107fe2374'
+BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [12345789, 'admin123', 5402859233]
+CHANNELS = [-1001953832687, --1001617807099, 'channelusername']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
