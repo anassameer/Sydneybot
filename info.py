bf3b7b4c0e92f40980fcdf12ad02078b7cc5339e
anassameer <139580401+anassameer@users.noreply.github.com>
2023-12-27 16:22:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Sydney')
 API_ID = int(environ.get('API_ID', '20876635'))
 API_HASH = environ.get('API_HASH', '8600379efa072b446f9bc81da4f40786')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6433432950:AAGKJNYGGQF1FR_oJ4cZJfBHCcUInIqvJGc')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6549985777:AAGKaCEbEevg5lyZ01GiatIr_HsyLyVAeMA')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
